e33c781cc7b30fc2f198aa3d1cc35d15b6dec487
Abhuday Singh <titancodeit@gmail.com>
2018-05-06 21:42:49 +0530

    Update

--- rail.py
+++ rail.py
@@ -6,9 +6,16 @@ url=url+train_no+"/date/"
 date=input("date <dd-mm-yyyy> : ")
 url=url+date+"/apikey/y3clwapnm8/"
 json_data=requests.get(url).json()
-position=json_data['position']
-
-print('Current Status: ',position)
-
-
 print(json_data)
+print(type(json_data))
+print()
+print(json_data['train'])
+print(type(json_data['train']))
+print(type(json_data['train']['days']))
+for day in json_data['train']['days']:
+    print(day['code']," ",day['runs'])
+
+print()
+print(type(json_data['route']))
+for r in json_data['route']:
+    print(r['station']['name'],": ",r['status'],": ",r['scharr'],": ",r['schdep'])
